01a8ec5fa249d7c1e4abdd391ec2d44e23290959
djedai <119414612+djedaisd@users.noreply.github.com>
2024-11-14 18:20:41 +0500

    Update ser.py

--- ser.py
+++ ser.py
@@ -5,35 +5,35 @@ import librosa
 from transformers import pipeline
 import threading
 
-# Инициализируем модель для классификации аудио
+
 pipe = pipeline("audio-classification", model="Aniemore/wavlm-emotion-russian-resd")
 
 # Флаг для остановки потока
 stop_thread = False
 
-# Функция для записи аудио с микрофона
+
 def record_audio(duration=5, samplerate=16000):
-    # Запись аудио с микрофона
+    
     print("Recording...")
     audio_data = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1, dtype='float32')
-    sd.wait()  # Дождаться окончания записи
-    return audio_data.flatten()  # Возвращаем данные как одномерный массив
+    sd.wait()  
+    return audio_data.flatten() 
 
 # Функция для распознавания эмоций
 def recognize_emotions():
     global stop_thread
     while not stop_thread:
-        # Запись аудио каждые 5 секунд
+        
         audio_data = record_audio(duration=5)
         
-        # Обработка аудио и предсказание эмоций
+        
         predictions = pipe(audio_data)
         
-        # Выводим предсказания в консоль
+        
         print(f"Predictions: {predictions}")
         
-        time.sleep(5)  # Пауза 5 секунд перед следующим предсказанием
-
+        time.sleep(5)  
+        
 # Запуск потока для распознавания
 def start_recognition():
     # Используем поток для выполнения функции recognize_emotions параллельно
@@ -46,11 +46,11 @@ def stop_recognition():
     stop_thread = True
     print("Recognition stopped.")
 
-# Запуск программы
+
 if __name__ == "__main__":
     start_recognition()
     
-    # Ожидание команды для остановки
+   
     try:
         while True:
             user_input = input("Press 'q' to stop: ")
